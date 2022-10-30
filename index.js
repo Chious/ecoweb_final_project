1f2c0cb84a6b4ad83eef2f70cf97a1b80e12f267
邱佳昇 <chu23294752@gmail.com>
2022-10-30 19:45:29 +0800

    update

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ $("h1").css("background-color","red")
 
 let data = 5;
 
+
+ 
+
 $(function (){
     $("#app-title").text("經濟系器材借閱系統");
     $("#myapp").append(Ebookcard());
@@ -13,17 +16,27 @@ $(function (){
     $("#myapp > div > div.ebookcard-pill > div.ebookcard-pill-availability > div.ebookcard-pill-availability-icon").append(`<i id="check" class="fa-solid fa-check"></i>`)
 
     $("#myapp > div > div.ebookcard-pill > div.ebookcard-pill-availability > div.ebookcard-pill-availability-number").text(data + "/5");
-    $("#myapp > div > div.ebookcard-pill > div.ebookcard-pill-action > div").append(`<button id = "RentKindleButton">Add</button>`)
+    
+
+    $("#myapp > div > div.ebookcard-pill > div.ebookcard-pill-action > div").append(`<button id="btn">Click</button>`);
 })
+
 var RentKindle = async function() {
-  const uri = "https://script.google.com/macros/s/AKfycbzn3waeZi7mRk4TXvhgW-oxm7xDy6DR5mGYCEbMZiWd_MNtwiZS_Wk437ej7xAEMb5-/exec";
-  const response = await fetch(uri);
-  const data = await response.json();
-  $("#myapp > div > div.ebookcard-pill > div.ebookcard-pill-availability > div.ebookcard-pill-availability-number").text(data + "/5");
+ const uri = "https://script.google.com/macros/s/AKfycbzn3waeZi7mRk4TXvhgW-oxm7xDy6DR5mGYCEbMZiWd_MNtwiZS_Wk437ej7xAEMb5-/exec";
+ const response = await fetch(uri);
+ const data = await response.json();
+      $("#myapp > div > div.ebookcard-pill > div.ebookcard-pill-availability > div.ebookcard-pill-availability-number").text(data + "/5");
+    }
+
+ window.onload=function(){   
+
+var btn = document.getElementById('btn');
+
+btn.addEventListener('click', function(e){
+  RentKindle();
+}, false);
+
 }
-
-$("#RentKindleButton").on('click','.btn', RentKindle());
-
 
 function Ebookcard() {
 
